b097246de2cc16f3004fd3055eee94d7779cbd48
EdGruberman <ed@rjump.com>
2012-03-26 01:15:01 -0700

    CHG: Updated for latest Bukkit events

--- src/edgruberman/bukkit/endermancontrol/Main.java
+++ src/edgruberman/bukkit/endermancontrol/Main.java
@@ -1,27 +1,54 @@
 package edgruberman.bukkit.endermancontrol;
 
+import java.util.logging.Handler;
+import java.util.logging.Level;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import edgruberman.bukkit.messagemanager.MessageManager;
+public final class Main extends JavaPlugin implements Listener {
 
-public final class Main extends JavaPlugin {
-    
-    static MessageManager messageManager;
-    
-    @Override
-    public void onLoad() {        
-        Main.messageManager = new MessageManager(this);
-        Main.messageManager.log("Version " + this.getDescription().getVersion());
-    }
-	
     @Override
     public void onEnable() {
-        new Enforcer(this);
-        Main.messageManager.log("Plugin Enabled");
+        this.setLoggingLevel();
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
-    
-    @Override
-    public void onDisable() {
-        Main.messageManager.log("Plugin Disabled");
+
+    private void setLoggingLevel() {
+        this.reloadConfig();
+        final String name = this.getConfig().getString("logLevel", "INFO");
+        Level level;
+        try { level = Level.parse(name); } catch (final Exception e) {
+            level = Level.INFO;
+            this.getLogger().warning("Unrecognized java.util.logging.Level in \"" + this.getDataFolder().getPath() + "\\config.yml\"; logLevel: " + name);
+        }
+
+        // Only set the parent handler lower if necessary, otherwise leave it alone for other configurations that have set it.
+        for (final Handler h : this.getLogger().getParent().getHandlers())
+            if (h.getLevel().intValue() > level.intValue()) h.setLevel(level);
+
+        this.getLogger().setLevel(level);
+        this.getLogger().log(Level.CONFIG, "Logging level set to: " + this.getLogger().getLevel());
     }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityChangeBlock(final EntityChangeBlockEvent change) {
+        if (change.getEntityType() != EntityType.ENDERMAN) return;
+
+        if (this.getLogger().isLoggable(Level.FINER))
+            this.getLogger().log(Level.FINER, "Denying Enderman changing block"
+                    + " from " + change.getBlock().getType().name()
+                    + " to " + change.getTo().name()
+                    + " at"
+                    + " x:" + change.getBlock().getX()
+                    + " y:" + change.getBlock().getY()
+                    + " z:" + change.getBlock().getZ()
+            );
+
+        change.setCancelled(true);
+    }
+
 }
